c2088e70a5d8dc42083d3ad001c8deabef8b26fd
KievTrung <kieutrung0701@gmail.com>
2025-08-04 10:47:53 +0700

    'mongo-s3-integrated'

--- server/server.py
+++ server/server.py
@@ -1,13 +1,26 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from utils.mongo import MongoDbClient
+from utils.s3 import S3Client
+import configparser
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/home", methods=["GET"])
+@app.route("/index", methods=["GET"])
 def home():
     return jsonify({"hello": "world"})
 
+def main():
+    config = configparser.RawConfigParser()
+    config.read('./config.ini')
+
+    # test connect s3
+    S3Client(config)
+    # test connect mongo
+    MongoDbClient(config)
+    
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    main()
