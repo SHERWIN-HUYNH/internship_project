a20e9ce1dd1a0dd91e75d2f3571aa0969ad37ae2
sherwin-huynh <huynhchitrung020503@gmail.com>
2025-08-05 14:06:44 +0700

    Upload UI/UX design

---
+++ client/reunite-face/components/PasswordInput.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { EyeIcon, EyeOffIcon } from 'lucide-react'
+
+interface PasswordInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  showPassword?: boolean
+}
+const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInputProps>(
+  // eslint-disable-next-line react/prop-types
+  ({ className, value, onChange, ...field }, ref) => {
+    const [showPassword, setShowPassword] = React.useState(false)
+
+    // eslint-disable-next-line react/prop-types
+    const disabled = value === '' || value === undefined || field.disabled
+
+    return (
+      <div className="relative">
+        <Input
+          type={showPassword ? 'text' : 'password'}
+          className={`hide-password-toggle pr-10 ${className}`}
+          ref={ref}
+          value={value}
+          onChange={onChange}
+          {...field}
+        />
+        <Button
+          type="button"
+          variant="ghost"
+          size="sm"
+          className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+          onClick={() => setShowPassword((prev) => !prev)}
+          disabled={disabled}
+        >
+          {showPassword && !disabled ? (
+            <EyeIcon className="h-4 w-4" aria-hidden="true" />
+          ) : (
+            <EyeOffIcon className="h-4 w-4" aria-hidden="true" />
+          )}
+          <span className="sr-only">
+            {showPassword ? 'Hide password' : 'Show password'}
+          </span>
+        </Button>
+
+        {/* Ẩn chức năng bật tắt mật khẩu mặc định của trình duyệt */}
+        <style>{`
+          .hide-password-toggle::-ms-reveal,
+          .hide-password-toggle::-ms-clear {
+            visibility: hidden;
+            pointer-events: none;
+            display: none;
+          }
+        `}</style>
+      </div>
+    )
+  },
+)
+
+PasswordInput.displayName = 'PasswordInput'
+
+export { PasswordInput }
---
+++ client/reunite-face/components/forms/RegisterForm.tsx
@@ -0,0 +1,121 @@
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form } from '@/components/ui/form'
+import 'react-phone-number-input/style.css'
+import CustomFormField, { FormFieldType } from '../CustomFormField'
+import SubmitButton from '../SubmitButton'
+import { toast } from 'sonner'
+import { Label } from '../ui/label'
+import { PasswordInput } from '../PasswordInput'
+import React from 'react'
+import { RegisterUser2 } from '@/validation/register'
+export const RegisterForm = () => {
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+  const [currentPassword, setCurrentPassword] = useState('')
+  const form = useForm<z.infer<typeof RegisterUser2>>({
+    resolver: zodResolver(RegisterUser2),
+    defaultValues: {
+      username: '',
+      email: '',
+      phone: '',
+    },
+  })
+  const onSubmit = async (values: z.infer<typeof RegisterUser2>) => {
+    console.log(form.formState.errors)
+    console.log('IS LOADING', isLoading)
+    setIsLoading(true)
+    try {
+      console.log('VALUES FROM REGISTER', values)
+      if (!currentPassword) {
+        throw new Error('Password is required')
+      }
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: values.email,
+          password: currentPassword,
+          username: values.username,
+          phone: values.phone,
+        }),
+      })
+      const responseData = await res.json()
+
+      if (!res.ok) {
+        throw new Error(responseData.error)
+      }
+
+      toast.success('Registered successfully')
+      router.push('/login')
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log('ERROR HAPPEN')
+        toast.error(error.message)
+      }
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  console.log(form.formState.errors)
+  console.log(isLoading)
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Xin chào 👋</h1>
+          <p className="text-dark-700">
+            Bước đầu của sức khỏe tốt hơn – Đặt lịch hẹn ngay hôm nay!
+          </p>
+        </section>
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="username"
+          label="Họ và tên"
+          placeholder="Ngô Thị Duyên"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="ngothiduyencute@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="phone"
+          label="Số điện thoại"
+          placeholder="(555) 123-4567"
+          type="number"
+        />
+        <div className="space-y-2 flex-1 mt-2">
+          <Label htmlFor="password" className="shad-input-label ">
+            Mật khẩu
+          </Label>
+          <PasswordInput
+            id="password"
+            value={currentPassword}
+            onChange={(e) => setCurrentPassword(e.target.value)}
+            autoComplete="password"
+          />
+        </div>
+
+        <SubmitButton isLoading={isLoading}>Đăng ký tài khoản </SubmitButton>
+      </form>
+    </Form>
+  )
+}
