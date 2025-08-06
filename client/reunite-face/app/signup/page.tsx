7ff02a3bec2ed35f4d5280623ae9ef6f340afd6d
sherwin-huynh <huynhchitrung020503@gmail.com>
2025-08-06 11:01:21 +0700

    UI/UX: sign up

--- client/reunite-face/app/signup/page.tsx
+++ client/reunite-face/app/signup/page.tsx
@@ -1,20 +1,20 @@
 import Image from 'next/image'
 import Link from 'next/link'
-
-import { LoginForm } from '@/components/forms/LoginForm'
 import React from 'react'
-const SignIn = () => {
+import { RegisterForm } from '@/components/forms/RegisterForm'
+
+const SignUp = () => {
   return (
     <div className="flex h-screen max-h-screen bg-primary">
       <section className="remove-scrollbar container my-auto ">
         <div className="sub-container max-w-[496px] bg-white p-10 rounded-xl">
-          <LoginForm />
+          <RegisterForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
               Reunite Face
             </p>
-            <Link href="/signup" className="text-green-500">
+            <Link href="/register" className="text-green-500">
               Đăng ký
             </Link>
           </div>
@@ -32,4 +32,4 @@ const SignIn = () => {
   )
 }
 
-export default SignIn
+export default SignUp
--- client/reunite-face/lib/validation.ts
+++ client/reunite-face/lib/validation.ts
@@ -3,3 +3,9 @@ export const UserLogin = z.object({
   email: z.string().email('Invalid email address'),
   password: z.string(),
 })
+
+export const RegisterUser = z.object({
+  username: z.string().min(2, NAME_LENGTH),
+  email: z.string().email(INVALID_EMAIL),
+  phone: z.string().min(10, MIN_LENGTH_PHONE).max(11, MAX_lENGTH_PHONE),
+})
